544579f033d499ceb4bbf73fa2f57fa23a1850dc
Павел <PaulEfimov2001@yandex.ru>
2020-08-07 03:03:14 +0300

    login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,70 +1,78 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 import {
     StyleSheet,
-    Text,
     View,
     TextInput,
-    ScrollView
+    ScrollView,
+    Alert,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Container from '../../components/Container';
-import Button from '../../components/Button';
-import Label from '../../components/Label';
+import {
+    Button,
+    Input,
+    Text,
+    Icon,
+} from 'react-native-elements';
 
-export default class Login extends Component{
-    render(){
+export default class Login extends Component {
+    render() {
         return (
             <ScrollView style={styles.scroll}>
-
-                <Container>
-                    <Text style={[styles.appName, styles.textCenter]}>
-                        Акцион
-                    </Text>
-                    <Text style={[styles.info, styles.textCenter]}>
-                        Привет!
-                    </Text>
-                </Container>
-
-                <Container>
-                        <View style={styles.row_container}>
-                            <Icon.Button name="apple" size={30} backgroundColor={'black'}/>
-                            <Icon.Button name="vk" size={30}/>
-                            <Icon.Button name="google-plus-square" size={30} backgroundColor={'red'}/>
-                        </View>
-                </Container>
-
-                <Container>
-                    <TextInput
-                        placeholder={"Email adress"}
-                        style={styles.textInput}
+                {/*Текст сверху*/}
+                <Text style={styles.textIntro}>Акцион</Text>
+                <Text h2 style={styles.textIntro}>Привет!</Text>
+                {/*Форма логина*/}
+                {/*Ввод почты*/}
+                <Input
+                    placeholder="Email"
+                    keyboardType={'email-address'}
+                    autoCorrect={false}
+                    leftIcon={
+                        <Icon
+                            name="user"
+                            type="font-awesome"
+                            size={24}
+                            color="black"/>
+                    }
+                />
+                {/*Ввод пароля*/}
+                <Input
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    leftIcon={
+                        <Icon
+                            name="lock"
+                            type="font-awesome"
+                            size={24}
+                            color="black"/>
+                    }
+                />
+                {/*Кнопка входа*/}
+                <Button
+                    title="Log in"
+                />
+                {/*Забыли пароль*/}
+                <Button
+                    title="Forgot password?"
+                    type="clear"
+                    buttonStyle={styles.alignRight}
+                />
+                {/*Иконки снизу*/}
+                <View style={styles.iconRow}>
+                    <Icon
+                        reverse
+                        name="vk"
+                        type="font-awesome"
+                        color="#597da3"
                     />
-                </Container>
-                <Container>
-                    <TextInput
-                        placeholder={"Password"}
-                        secureTextEntry={true}
-                        style={styles.textInput}
-                    />
-                </Container>
-
-                <Container>
-                    <Button
-                        label="Forgot Password?"
-                        styles={{button: styles.alignRight, label: styles.label}}
-                        //onPress={this.press.bind(this)}
-                    />
-                </Container>
-
-                <Container>
-                    <Button
-                        label="Sign In"
-                        styles={{button: styles.primaryButton, label: styles.buttonWhiteText}}
-                        //onPress={this.press.bind(this)}
-                    />
-                </Container>
-
+                    <Icon
+                        reverse
+                        name="google-plus-square"
+                        type="font-awesome"
+                        color="#d34836"/>
+                </View>
             </ScrollView>
-        )
+        );
     }
 }
 
@@ -72,67 +80,21 @@ const styles = StyleSheet.create({
     scroll: {
         backgroundColor: '#ffffff',
         padding: 30,
-        flexDirection: 'column'
+        flexDirection: 'column',
     },
-    textInput: {
-        height: 50,
-        fontSize: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#939191',
-        backgroundColor: '#FFF'
-    },
-    transparentButton: {
-        marginTop: 30,
-        borderColor: '#3B5699',
-        borderWidth: 2
-    },
-    buttonBlueText: {
-        fontSize: 20,
-        color: '#3B5699'
-    },
-    buttonBigText: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    inline: {
-        flexDirection: 'row'
-    },
-    buttonWhiteText: {
-        fontSize: 20,
-        color: '#000000',
-        fontWeight: 'bold'
-    },
-    buttonBlackText: {
-        fontSize: 20,
-        color: '#595856'
-    },
-    primaryButton: {
-        backgroundColor: '#9e9e9e'
-    },
-    appName: {
-        fontSize: 12,
-        color: '#000000',
-        marginBottom: 20,
-        fontStyle: 'italic'
-    },
-    info:{
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    textCenter:{
+    //Стиль текста сверху
+    textIntro: {
         textAlign: 'center',
     },
-    label: {
-        color: '#0d8898',
-        fontSize: 10
-    },
+    //Стиль кнопки забыли пароль
     alignRight: {
-        alignSelf: 'flex-end'
+        alignSelf: 'flex-end',
     },
-    row_container: {
+    //Стиль для иконок в ряд
+    iconRow: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        alignItems: 'center',
+        justifyContent: 'space-around',
     },
 });
-
