534db1bfd67b83e144ed5b876c1d19bf8df924e7
blakexdd <blakexxxd@gmail.com>
2020-08-08 00:24:40 +0300

    Tasks todo added

--- core/util/log.js
+++ core/util/log.js
@@ -1,6 +1,9 @@
+/* TODO: Need to setup logger. Most important to set the colors up
+*   so it is comfortable to read logs
+*   Here you can find documentation */
 const {logger} = require('react-native-logs');
 const log = logger.createLogger();
 
-module.exports ={
+module.exports = {
     logger: log
 }
