b96479f2c4b0a2a3b05d4133fe84ba2bb14463a5
vanoha <vanoha@mail.ru>
2020-08-13 19:49:43 +0300

    company_page

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Button } from 'react-native';
 import Login from './src/pages/login';
 import Signup from "./src/pages/signup";
-import Company_profile from "./src/pages/company_profile"
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,12 +11,13 @@ const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
-      <NavigationContainer>
+      <Company_profile/>
+      /*<NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Signup" component={Signup} />
         </Stack.Navigator>
-      </NavigationContainer>
+      </NavigationContainer>*/
   );
 };
 
--- src/pages/company_profile.js
+++ src/pages/company_profile.js
@@ -1,5 +1,6 @@
 import {logger} from "react-native-logs";
-import {Alert} from "react-native";
+import {Alert, Button, ListView} from 'react-native';
+import ReactList from 'react-list'
 
 const React = require('react');
 const {Component} = require('react');
@@ -15,6 +16,38 @@ const {
     Icon,
     ListItem,
 } = require('react-native-elements');
+
+const Id_feature = [
+    {
+        id: 0,
+        text: "Кофе",
+    },
+    {
+        id: 1,
+        text: "Ресторан",
+    },
+    {
+        id: 2,
+        text: "Бар",
+    }
+]
+
+const Comments = [
+    {
+        id: 0,
+        user: 'Пользователь 1',
+        data: '12.08.20',
+        rating: "Рейтинг",
+        content: "Краткое описание акции, ее оценка и может быть рекомендация другим пользователям использовать ее пока она действует"
+    },
+    {
+        id: 1,
+        user: 'Пользователь 2',
+        data: '16.10.19',
+        rating: "Рейтинг",
+        content: "Краткое описание акции, ее оценка и может быть рекомендация другим пользователям использовать ее пока она действует"
+    },
+]
 
 const Auctions = [
     {
@@ -52,19 +85,49 @@ export default class Company_profile extends Component {
                 {/*Компания*/}
                 <ListItem
                     leftAvatar={{
-                        size: 'small',
+                        size: 'large',
                         source: {
                             uri: 'https://cdn1.zp.ru/job/attaches/2018/09/70/97/709707a9493e933767019d6aafb66e7a.jpg'
                         }
                     }}
+
                     titleStyle={{fontSize: 20}}
                     title="CoffeeLike"
                     subtitle="Рейтинг"
+                    style={styles.list_item}
                 />
-                <Text
-                    onPress={}
-                    >Адрес (на карте)</Text>
-                <Text h4>Избранные акции ({Auctions.length})</Text>
+                {/*Адрес*/}
+                <Text style={styles.text_under_title}>
+                    Адрес (на карте)
+                </Text>
+                {/*Расстояние*/}
+                <Text style={styles.text}>
+                    Расстояние (время на дорогу)
+                </Text>
+                <Text style={styles.text}>
+                    Расстояние (время на дорогу)
+                </Text>
+                {/*Вид компании*/}
+                <View style={{alignContent: 'center', flexDirection: 'row', justifyContent: 'space-around', paddingTop: 20}}>
+                    <Text style={styles.text_id_feature}>Кафе</Text>
+                    <Text style={styles.text_id_feature}>Бар</Text>
+                    <Text style={styles.text_id_feature}>Ресторан</Text>
+                </View>
+                {/*Кнопка подписаться*/}
+                <Text style={{
+                    marginTop: 17,
+                    color: "#000",
+                    fontSize: 20,
+                    borderWidth: 10,
+                    borderColor: '#b8b8b8',
+                    borderRadius: 7,
+                    textAlign: 'center',
+                    textAlignVertical: 'center',
+                    backgroundColor: '#b8b8b8',
+                    marginBottom: 23,
+                }}>Подписаться</Text>
+                <Text h4>Акции</Text>
+                {/*Акции*/}
                 <ScrollView horizontal>
                     <FlatList
                         numColumns={Auctions.length}
@@ -74,16 +137,31 @@ export default class Company_profile extends Component {
                         }
                     />
                 </ScrollView>
-                <Text h4>Избранные компании ({Auctions.length})</Text>
-                <ScrollView horizontal>
-                    <FlatList
-                        numColumns={Auctions.length}
-                        data={Auctions}
-                        renderItem={({item}) =>
-                            (<Text style={styles.textList}>{item.title}</Text>)
-                        }
-                    />
-                </ScrollView>
+                {/*Разделитель*/}
+                <Text style={{backgroundColor: '#b8b8b8', height: 2, marginTop: 15, marginBottom: 15}}/>
+                <Text h4>Комментарии</Text>
+                {/*Комментарии*/}
+                <FlatList
+                    data={Comments}
+                    renderItem={({item}) =>
+                        (
+                            /*<Text style={styles.textList}>{item.title}</Text>*/
+                            <ListItem
+                                title={item.user}
+                                rightTitle={item.data}
+                                rightTitleStyle={{alignItems: 'flex-start'}}
+                                subtitle={
+                                    <View>
+                                        <Text>{item.rating}</Text>
+                                        <Text>{item.content}</Text>
+                                    </View>
+                                }
+                                bottomDivider="true"
+                            />
+                        )
+                    }
+                />
+                {/*Список: настройки и выход*/}
                 <ListItem
                     title="Статистика"
                     rightIcon={
@@ -98,17 +176,6 @@ export default class Company_profile extends Component {
                     onPress={() => Alert.alert("Test")}
                 />
                 <ListItem
-                    title="Настройки"
-                    rightIcon={
-                        <Icon
-                            name="gear"
-                            type="font-awesome"
-                        />
-                    }
-                    bottomDivider={1}
-                    onPress={() => Alert.alert("Test")}
-                />
-                <ListItem
                     title="Выйти"
                     rightIcon={
                         <Icon
@@ -118,6 +185,7 @@ export default class Company_profile extends Component {
                     }
                     bottomDivider={1}
                     onPress={() => Alert.alert("Test")}
+                    style={{marginBottom: 50}}
                 />
             </ScrollView>
         );
@@ -125,11 +193,36 @@ export default class Company_profile extends Component {
 }
 
 const styles = StyleSheet.create({
+    text_under_title: {
+        marginLeft: 106,
+        marginTop: -29,
+        color: "#7a7a7a"
+    },
+    text_id_feature: {
+        color: "#fff",
+        height: 25,
+        fontSize: 15,
+        borderWidth: 10,
+        borderColor: '#b8b8b8',
+        borderRadius: 7,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        backgroundColor: '#b8b8b8',
+    },
+    text: {
+        marginLeft: 106,
+        color: "#7a7a7a",
+    },
+    list_item: {
+    },
     scroll: {
         backgroundColor: '#ffffff',
         padding: 30,
         flexDirection: 'column',
     },
+    /*textList0: {
+        backgroundImage: Rectangle,
+    },*/
     textList: {
         backgroundColor: '#95a5a6',
         marginTop: 5,
