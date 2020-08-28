2a01d58e93c0a890e0c9b71d5dc8c244860dfef3
vanoha <vanoha@mail.ru>
2020-08-29 00:57:20 +0300

    main page is ready

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,5 +1,6 @@
 import {Image} from "react-native-elements";
-import {ImageBackground} from "react-native";
+import {Button, FlatList, SafeAreaView} from 'react-native';
+import {HeaderTitle} from '@react-navigation/stack';
 
 const React = require('react');
 const {Component} = require('react');
@@ -8,21 +9,32 @@ const {Text, ButtonGroup, Tile, ListItem, Card, Icon} = require('react-native-el
 
 const kindsOfShops = [
     {
+        id: 1,
         name: "Бар",
         imageUrl: {uri: "https://avatars.mds.yandex.net/get-zen_doc/135437/pub_5d4540e2027a1500adcef2f0_5d45444935c8d800ad0d594e/scale_1200"},
         count: 15
     },
     {
+        id: 2,
         name: "Ресторан",
         imageUrl: {uri: "https://sovcominvest.ru/uploads/products/155a40b4c3151476fbbfbbbfdc1b8c90.jpg"},
         count: 7
     },
     {
+        id: 3,
         name: "Кафе",
         imageUrl: {uri: "https://img2.goodfon.ru/original/2560x1600/0/4f/zermatt-switzerland-cermatt-6874.jpg"},
         count: 2
     }
 ];
+
+const Shops = [
+    {
+        name: "Макдональдс",
+        imageUrl: {uri: "http://sc-globalcity.ru/sites/default/files/offers/800x800_site_mccombo_bm.jpg"},
+        kinds: ["Bar", "Restaurant", "Cafe"]
+    }
+]
 
 export default class Main extends Component {
     constructor() {
@@ -48,11 +60,16 @@ export default class Main extends Component {
 
         const {selectedIndex} = this.state
 
+        const Item = ({name, imageUrl, kinds}) => (
+            <ImageBackground source={imageUrl}
+            style={{width: 20, height: 20}}>
+
+            </ImageBackground>
+        );
+
         return (
-            <ScrollView style={{
-                backgroundColor: '#ffffff',
-                padding: 10,
-            }}>
+            <View style={{flex: 1}}>
+                <View style={{padding: 10, backgroundColor: "#fff"}}>
                 <ButtonGroup
                     onPress={this.updateIndex}
                     selectedIndex={selectedIndex}
@@ -100,24 +117,115 @@ export default class Main extends Component {
                         ))
                     }
                 </ScrollView>
-
+                </View>
+                <ScrollView
+                style={{flex: 1}}>
                 <View style={{borderColor: "black"}}>
                     <ListItem
                         key={0}
                         leftAvatar={{source: {uri: "http://vsefranshizi.ru/cms/cms-images/publication_item/0137/image/McDonalds.jpg"}}}
                         rightIcon={<Icon
-                            name="angle-right"
+                            name="bars"
                             type="font-awesome"
                         />}
                         title={"Макдоналс"}
-                        subtitle={"Мак"}
+                        subtitle={"0.8 км"}
                     />
-                    <ImageBackground
-                        source={{uri: "http://sc-globalcity.ru/sites/default/files/offers/800x800_site_mccombo_bm.jpg"}}
-                        style={{flex: 1, width: "100%", height: 350}}
-                    />
+                    <ImageBackground source={{uri: "http://vsefranshizi.ru/cms/cms-images/publication_item/0137/image/McDonalds.jpg"}}
+                        style={{width: "100%", height: 350}}>
+                        <FlatList
+                            data={kindsOfShops}
+                            horizontal={true}
+                            renderItem={({item}) =>
+                                <View
+                                style={{
+                                    padding: 4
+                                }}>
+                                <Text style={{
+                                    height: 20,
+                                    borderColor: "#b0b0b0",
+                                    borderRadius: 4,
+                                    textAlignVertical: 'center',
+                                    borderWidth: 15,
+                                    textAlign: 'center',}}
+                                >{item.name}</Text>
+                                </View>
+                            }
+                            style={{
+                                alignSelf: 'flex-end',
+                                paddingTop: 4
+                            }}
+                        />
+                        <View style={{
+                            alignSelf: 'stretch',
+                            margin: 20,
+                        }}>
+                            <Text style={{
+                                fontSize: 20,
+                                fontWeight: 'bold'
+                            }}>Акция на бургер</Text>
+                            <Text style={{
+                                fontSize: 18,
+                                color: "#5d5c5c",
+                                fontWeight: 'bold'
+                            }}>27.08.2020 - 31.12.2020</Text>
+                        </View>
+                    </ImageBackground>
                 </View>
-            </ScrollView>
-        )
+                    <View style={{borderColor: "black"}}>
+                        <ListItem
+                            key={0}
+                            leftAvatar={{source: {uri: "http://vsefranshizi.ru/cms/cms-images/publication_item/0137/image/McDonalds.jpg"}}}
+                            rightIcon={<Icon
+                                name="bars"
+                                type="font-awesome"
+                            />}
+                            title={"Макдоналс"}
+                            subtitle={"0.8 км"}
+                        />
+                        <ImageBackground source={{uri: "http://vsefranshizi.ru/cms/cms-images/publication_item/0137/image/McDonalds.jpg"}}
+                                         style={{width: "100%", height: 350}}>
+                            <FlatList
+                                data={kindsOfShops}
+                                horizontal={true}
+                                renderItem={({item}) =>
+                                    <View
+                                        style={{
+                                            padding: 4
+                                        }}>
+                                        <Text style={{
+                                            height: 20,
+                                            borderColor: "#b0b0b0",
+                                            borderRadius: 4,
+                                            textAlignVertical: 'center',
+                                            borderWidth: 15,
+                                            textAlign: 'center',}}
+                                        >{item.name}</Text>
+                                    </View>
+                                }
+                                style={{
+                                    alignSelf: 'flex-end',
+                                    paddingTop: 4
+                                }}
+                            />
+                            <View style={{
+                                alignSelf: 'stretch',
+                                margin: 20,
+                            }}>
+                                <Text style={{
+                                    fontSize: 20,
+                                    fontWeight: 'bold'
+                                }}>Акция на бургер</Text>
+                                <Text style={{
+                                    fontSize: 18,
+                                    color: "#5d5c5c",
+                                    fontWeight: 'bold'
+                                }}>27.08.2020 - 31.12.2020</Text>
+                            </View>
+                        </ImageBackground>
+                    </View>
+                </ScrollView>
+            </View>
+        );
     }
 }
